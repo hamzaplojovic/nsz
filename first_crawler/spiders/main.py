565177099130e05d554a79af0a7c7204bc3fab8c
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-12-12 18:34:24 +0100

    NZS Crawler

--- first_crawler/spiders/main.py
+++ first_crawler/spiders/main.py
@@ -1,4 +1,5 @@
 with open("index.txt") as f:
         data = f.readlines()
+        data2 = str(data)
         with open("index2.txt", 'w') as f:
-                f.write(str(data))
+                f.write(str(data2))
