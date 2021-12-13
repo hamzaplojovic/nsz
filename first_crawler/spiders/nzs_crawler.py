7b98b25d93062356b30da641383d41986e2204cd
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-12-13 14:48:56 +0100

    New Crawler

--- first_crawler/spiders/nzs_crawler.py
+++ first_crawler/spiders/nzs_crawler.py
@@ -4,8 +4,8 @@ import scrapy
 class NszSpider(scrapy.Spider):
     name = 'nsz'
     allowed_domains = ['www.nsz.gov.rs']
-    start_urls = ['http://http://www.nsz.gov.rs/employee/jobs/search?page=1/',
-                  'http://http://www.nsz.gov.rs/employee/jobs/search?page=2/']
+    start_urls = ['http://www.nsz.gov.rs/employee/jobs/search?page=1/',
+                  'http://www.nsz.gov.rs/employee/jobs/search?page=2/']
 
     def parse(self, response):
         for job in response.css('div.single-job'):
