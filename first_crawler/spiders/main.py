473b02e6d9c744f6d5f9c23561a06b5e177be86c
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-12-13 14:43:26 +0100

    New spider

--- first_crawler/spiders/main.py
+++
@@ -1,5 +0,0 @@
-with open("index.txt") as f:
-        data = f.readlines()
-        data2 = str(data)
-        with open("index2.txt", 'w') as f:
-                f.write(str(data2))
--- first_crawler/spiders/nzs_crawler.py
+++ first_crawler/spiders/nzs_crawler.py
@@ -1,17 +1,13 @@
 import scrapy
 
 
-class NzsCrawlerSpider(scrapy.Spider):
-    name = 'nzs_crawler'
-    allowed_domains = ['http://www.nsz.gov.rs/live/trazite-posao/poslovi-oglasi?sid=Novi+Pazar&ctid=']
-    start_urls = ['http://www.nsz.gov.rs/live/trazite-posao/poslovi-oglasi?sid=Novi+Pazar&ctid=']
+class NszSpider(scrapy.Spider):
+    name = 'nsz'
+    allowed_domains = ['www.nsz.gov.rs']
+    start_urls = ['http://http://www.nsz.gov.rs/employee/jobs/search?page=1/',
+                  'http://http://www.nsz.gov.rs/employee/jobs/search?page=2/']
 
     def parse(self, response):
-        with open('index.txt', 'w') as f:
-            data = str(response.xpath('/html/body/div[1]/div/div/div[3]/div[1]/div[1]/div[2]/div/div/div').extract())
-            data = str(data[2:-2])
-            f.write(data)
-        with open("index.txt") as f:
-            lines = f.readlines()
-            stocks = [x.replace("\r\n","") for x in lines]
-            f.write(stocks)
+        for job in response.css('div.single-job'):
+            job.xpath('div[1]/@onclick').get()
+            pass
