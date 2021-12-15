b2fa7196b97792d80d5302b762ccdca20e493d25
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-12-15 14:37:21 +0100

    NSZ Crawler

--- first_crawler/spiders/nzs_crawler.py
+++ first_crawler/spiders/nzs_crawler.py
@@ -1,6 +1,6 @@
 import scrapy
 from bs4 import BeautifulSoup
-
+import json
 
 class NszSpider(scrapy.Spider):
     name = 'nsz'
@@ -9,12 +9,14 @@ class NszSpider(scrapy.Spider):
                   'http://www.nsz.gov.rs/employee/jobs/search?page=2/']
 
     def parse(self, response):
-        item = str(response.selector.xpath('/html/body/main/section/div/div/div/div').get())
-        with open("data.html", 'w') as w:
-            w.write(str(item))
-        with open("data.html", 'w') as f:
+        # with open("data.html", "w") as f:
+        #     f.write(str(response.selector.xpath('/html/body/main/section/div/div/div/div').get().split()))
+        with open("data2.json", 'w') as f:
             soup = BeautifulSoup(open("data.html"))
-            for tag in soup.find_all('p'):
-                f.write("<div>"+tag+"</div>")
+            divs = soup.find_all('h3')
+            h3 = {}
+            h3["h"] = str(divs)
+            f.write(str(h3))
+        
 
         
